575b2a8cd2daa1c90bba5f157a21356716145497
jasmine000719 <jasmine000719@163.com>
2021-07-16 11:29:04 +0800

    My Vue Demo

---
+++ src/components/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    用户名：<input type="text" width="22"/>
+    密码：<input type="password" width="22">
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>
